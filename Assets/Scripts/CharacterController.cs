1411670d602e1391b4348422ba28ffa3449e136d
Darin <darpaler@uat.edu>
2019-01-09 17:38:45 -0700

    Added Movement
    Added movement using animation root movement and a blend tree with a vertical and horizontal axis.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Pawn pawn;
+    public Vector3 moveVector = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    moveVector.x = Input.GetAxis("Horizontal");
+	    moveVector.z = Input.GetAxis("Vertical");
+        pawn.Move(moveVector);
+
+	}
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 direction)
+    {
+        anim.SetFloat("Vertical", direction.z);
+        anim.SetFloat("Horizontal", direction.x);
+    }
+}
