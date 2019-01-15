5295fbfb1a2e0b0126682086f1017733b6a98365
Darin <darpaler@uat.edu>
2019-01-14 17:26:17 -0700

    Added Follow Camera and Fixed Movement
    Added FollowGameObject Script to Main Camera and Fixed left, right, foward, and back run and walk animations.

---
+++ Assets/Scripts/FollowGameObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGameObject : MonoBehaviour {
+
+    //Variables
+    public Transform targetObjectTransform;
+    public Vector3 offset;
+
+    private Transform tf;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    tf = GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    tf.position = targetObjectTransform.position + offset;
+        tf.LookAt(targetObjectTransform.position);
+
+	}
+}
