b5ae82b54aba0790b2e8e6c374beadef8d1f597f
Darin <darpaler@uat.edu>
2019-01-30 21:51:02 -0700

    Made Code User Friendly

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     //Variables
+    [SerializeField, Tooltip("The pawn that the player controls.")]
     public Pawn pawn;                               //The Player's Pawn
-    public Vector3 moveVector = new Vector3();      //Our Movement Input
+    [SerializeField, Tooltip ("The player's movement input.")]
+    private Vector3 moveVector = new Vector3();     //Our Movement Input
+    [SerializeField, Tooltip("Object for testing where the mouse is.")]
     public Transform testObject;                    //Object For Testing Mouse Location
 
 	// Use this for initialization
@@ -22,11 +25,14 @@ public class CharacterController : MonoBehaviour
 	    Movement();
 	}
 
+    /// <summary>
+    /// Causes the pawn to face the mouse
+    /// </summary>
     void Rotation()
     {
-        Plane thePlane = new Plane(Vector3.up, pawn.tf.position);       //Plane Under Where Our Pawn is
+        Plane thePlane = new Plane(Vector3.up, pawn.Transform.position);       //Plane Under Where Our Pawn is
         Ray theRay = Camera.main.ScreenPointToRay(Input.mousePosition); //Where the Mouse is Over the Plane
-
+        
         //Get Where the Mouse Intersects
         float distance;
         thePlane.Raycast(theRay, out distance); 
@@ -40,6 +46,9 @@ public class CharacterController : MonoBehaviour
     }
 
     //Player Movement Input
+    /// <summary>
+    /// Gets the players movement input and sends it to the pawn
+    /// </summary>
     void Movement()
     {
         //Get Player Input
@@ -47,7 +56,7 @@ public class CharacterController : MonoBehaviour
         moveVector.z = Input.GetAxis("Vertical");
         
         //Use Local Space
-        moveVector = pawn.tf.InverseTransformDirection(moveVector);
+        moveVector = pawn.Transform.InverseTransformDirection(moveVector);
 
         //Make Sure Controller and Keyboard Move the Same Speed
         moveVector = Vector3.ClampMagnitude(moveVector, 1.0f);
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CapsuleCollider))]
+
 public class Pawn : MonoBehaviour
 {
 
     //Variables
-    public Transform tf;                        //Transform Component
+    [Header("Components")]
+    [SerializeField]
+    private Transform tf;                        //Transform Component
+    public Transform Transform
+    {get{return tf;}private set{tf = value;}}
+    [SerializeField]
     private Animator anim;                      //Animator Component
+    [SerializeField]
+    private CapsuleCollider capsuleCollider;    //CapsuleCollider Components
+
+    [Header("Movement Settings")]
+    [SerializeField, Range(0f, 500f), Tooltip("The speed the player moves.")]
     public float moveSpeed;                     //Player's Movement Speed
+    [SerializeField, Range(0f, 500f), Tooltip("The speed the player turns in degrees/second.")]
     public float turnSpeed;                     //Player's Turn Speed
+    [Header("Crouch Settings")]
+    [SerializeField, Tooltip("If the player is holding the crouch button.")]
     public bool isCrouching;                    //Is The Player Crouching
-    public CapsuleCollider capsuleCollider;     //CapsuleCollider Components
-    public Vector3 normalColliderCenter;        //The Center of The Player's Standing Collider
+    [SerializeField, Tooltip("The center of the player's standing collider.")]
+    public Vector3 normalColliderCenter;        //The Center of the Player's Standing Collider
+    [SerializeField, Range(0f, 25f), Tooltip("The height of the player's standing collider.")]
     public float normalColliderHeight;          //The Height of The Player's Standing Collider
+    [SerializeField, Tooltip("The center of the player's crouching collider.")]
     public Vector3 crouchColliderCenter;        //The Center of The Player's Crouching Collider
+    [SerializeField, Range(0f, 25f), Tooltip("The height of the player's crouching collider.")]
     public float crouchColliderHeight;          //The Height of The Player's Crouching Collider
 
 
@@ -34,6 +54,10 @@ public class Pawn : MonoBehaviour
 	}
 
     //Player Movement
+    /// <summary>
+    /// Sets the animator's Vertical, Horizontal, and Crouch variables, multiplied by the pawn's move speed.
+    /// </summary>
+    /// <param name="direction">The direction the player is moving in</param>
     public void Move(Vector3 direction)
     {
         //Tell The Animator How The Player is Moving
@@ -57,6 +81,10 @@ public class Pawn : MonoBehaviour
     }
 
     //Player Rotation
+    /// <summary>
+    /// Rotates the pawn towards a target point
+    /// </summary>
+    /// <param name="targetPoint">The target that the pawn will rotate towards</param>
     public void RotateTowards(Vector3 targetPoint)
     {
         //Find the Difference Between the Pawn and What We Want to Look At
