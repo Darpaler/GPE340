c9f8b8ac0f33f70f018e5ab8c3ad283ab5d2f2c5
Darin <darpaler@uat.edu>
2019-04-18 19:03:52 -0700

    Worked on Settings Scene

--- Assets/Scripts/ResolutionTest.cs
+++ Assets/Scripts/ResolutionTest.cs
@@ -5,33 +5,70 @@ using UnityEngine.UI;
 
 public class ResolutionTest : MonoBehaviour
 {
+    [Header("Audio Settings")]
+    public Slider masterVolumeSlider;               //Master Volume Slider
+    public Slider musicVolumeSlider;                //Music Volume Slider
+    public Slider sfxVolumeSlider;                  //SFX Volume Slider
 
-    public Dropdown resDropdown;
+    [Header("Video Settings")]
+    public Dropdown resDropdown;                    //Resolution Dropdown
+    public Toggle fullscreenToggle;
+    public Dropdown qualityDropdown;                //Quality Dropdown
+    List<string> resolutions = new List<string>();  //List of Resolutions
+    List<string> qualities = new List<string>();    //List of Qualities
 
-	// Use this for initialization
-	void Start ()
+    [Header("Menu Settings")]
+    public Button applyButton;                      //Apply Changes Button
+
+    // Use this for initialization
+    void Awake ()
 	{
-
-	    resDropdown = GetComponent<Dropdown>();
+        //Clear Dropdowns
         resDropdown.ClearOptions();
-        List<string> resolutioList = new List<string>();
+        qualityDropdown.ClearOptions();
 
-        /*
-	    for (int i = 0; i < Screen.resolutions.Length; i++)
-	    {
-            resolutioList.Add(Screen.resolutions[i].width + " x " + Screen.resolutions[i].height);
-	    }
-        */
+        //Set Lists
+        for (int index = 0; index < Screen.resolutions.Length; index++)
+        {
+            resolutions.Add(string.Format("{0} x {1}", Screen.resolutions[index].width, Screen.resolutions[index].height));
+        }
+        resDropdown.AddOptions(resolutions);
 
-        resolutioList = new List<string>(QualitySettings.names);
+        qualities = new List<string>(QualitySettings.names);
+        qualityDropdown.AddOptions(qualities);
 
-        resDropdown.AddOptions(resolutioList);
-
-        QualitySettings.SetQualityLevel(resDropdown.value);
 	}
 
-	// Update is called once per frame
-	void Update () {
-		
+    void OnEnable()
+    {
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("Master Volume", masterVolumeSlider.maxValue);
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("Music Volume", musicVolumeSlider.maxValue);
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFX Volume", sfxVolumeSlider.maxValue);
+        fullscreenToggle.isOn = Screen.fullScreen;
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        applyButton.interactable = false;
+    }
+
+    public void Apply()
+    {
+        PlayerPrefs.SetFloat("Master Volume", masterVolumeSlider.value);
+        PlayerPrefs.SetFloat("Music Volume", musicVolumeSlider.maxValue);
+        PlayerPrefs.SetFloat("SFX Volume", sfxVolumeSlider.maxValue);
+        string[] r = resDropdown.options[resDropdown.value].text.Split('x');
+        Screen.SetResolution(int.Parse(r[0].Trim()), int.Parse(r[1].Trim()), fullscreenToggle.isOn);
+        Debug.Log(r[0].Trim() + " x " + r[1].Trim());
+        Debug.Log(Screen.currentResolution);
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
+        applyButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	}
+
+    public void enableApply()
+    {
+        if (!applyButton.interactable) { applyButton.interactable = true; }
+    }
+
 }
